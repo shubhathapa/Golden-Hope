8ab15df78b41403db89f48c86e4517812b339593
Shubha Thapa <shubha7181@gmail.com>
2026-05-29 21:30:03 -0400

    Fix routing and display ProductFeed as home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./Cart";
 import ProductPage from "./ProductPage";
+import ProductFeed from "./ProductFeed";
 
 function App() {
-  return <Cart />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductFeed />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
