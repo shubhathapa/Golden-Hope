094d1952c920602460ba3ef45bb291bc20f7693c
Shubha Thapa <shubha7181@gmail.com>
2026-05-30 19:43:25 -0400

    fixed backend port and mongoose

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,11 @@ const app = express();
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection failed:", err));
 
-    app.get("/", (req, res) => {
-      res.send("Backend working");
-    });
+app.get("/", (req, res) => {
+  res.send("Backend working");
+});
 
-    app.listen(5000, () => {
-      console.log("Running on port 5000");
-    });
-  })
-  .catch(console.error);
+app.listen(5000, () => console.log("Running on port 5000"));
