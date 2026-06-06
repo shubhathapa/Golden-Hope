cce5b0dae981d87101297640d528b0946d1e03f4
May Theint Htet <maytheinthtet98.mm@gmail.com>
2026-06-06 15:21:02 -0400

    Fix duplicate Login import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,7 @@ import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ProtectedRoute from './components/ProtectedRoute'
 import Login from "./pages/Login";    //m
-import './App.css'
-import Login from "./pages/Login";    //m
+import './App.css';
 import ContactPage from "./pages/ContactPage";  //m
 
 function App() {
@@ -27,13 +26,7 @@ function App() {
         <Route path="/cart" element={<ProtectedRoute><Cart/></ProtectedRoute>} />
         <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
         <Route path="/about" element={<AboutPage />} />
-<<<<<<< HEAD
-        <Route path="/" element={<h1>HOME PAGE</h1>} />
-        <Route path="/login" element={<h1>LOGIN PAGE</h1>} />
-        <Route path="/contact" element={<ContactPage />} />        //m
-=======
         <Route path="/login" element={<Login />} />
->>>>>>> 9eb95fe79dcee717c21b79e6c016f9cb1e0a3f65
       </Routes>
     </>
   )
