2def0d6f671f0ab8f9cbf954b04b63cc70a0f1cf
Jiaxin Lei <leijiaxin0325@gmail.com>
2026-06-06 16:14:47 -0400

    Remove the protected for cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./components/navbar"
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ProtectedRoute from './components/ProtectedRoute'
-import Login from "./pages/Login";    //m
+import Login from "./pages/Login"; 
 import './App.css'
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Route path="/success" element={<PaymentSuccess />} />
         <Route path="/cancel" element={<PaymentCancel />} />
         <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/cart" element={<ProtectedRoute><Cart/></ProtectedRoute>} />
+        <Route path="/cart" element={<Cart/>} />
         <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<Login />} />
